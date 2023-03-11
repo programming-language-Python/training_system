ac37072d74388abd69849f353bc2e47cbf101aca
Roman <maks3564195@gmail.com>
2023-03-11 19:11:37 +0300

    Тестирование деплоя

--- src/config/settings.py
+++ src/config/settings.py
@@ -130,14 +130,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'config/static',
     BASE_DIR / 'testing/static',
 ]
 
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = BASE_DIR / 'static'
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
