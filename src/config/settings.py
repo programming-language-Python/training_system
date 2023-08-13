016b0a64d3c037f604426c2a9b4f9237fa2b4c9c
Roman <maks3564195@gmail.com>
2023-08-13 10:46:58 +0300

    Тестирование деплоя

--- src/config/settings.py
+++ src/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 'RENDER' in os.environ
 
 ALLOWED_HOSTS = []
 
