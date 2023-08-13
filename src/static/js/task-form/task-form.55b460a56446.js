1d9a6fa6a3e16fbd22d58c2ab32e5bfff109db44
Roman <maks3564195@gmail.com>
2023-08-13 13:40:21 +0300

    Тестирование деплоя

---
+++ src/static/js/task-form/task-form.55b460a56446.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+    const TIMEOUT = 500;
+    $("#task-list").on("click", ".btn-update", () => {
+        setTimeout(blockOrUnblockFields, TIMEOUT);
+    });
+    $("#testing").on("click", "#btn-add-task", () => {
+        setTimeout(blockOrUnblockFields, TIMEOUT);
+    });
+
+    function blockOrUnblockFields() {
+        let $isIfOperator = $('#id_is_if_operator'),
+            $conditionOfIfOperator = $('#id_condition_of_if_operator'),
+            $cycle = $('#id_cycle'),
+            $cycleCondition = $('#id_cycle_condition'),
+            $operatorNesting = $('#id_operator_nesting'),
+            $isOOP = $('#id_is_OOP'),
+            $isStrings = $('#id_is_strings'),
+
+            checkboxCycle = $cycle.find('input:checkbox'),
+
+            valueIsIfOperator = $isIfOperator.val(),
+            firstSelectValue = $isIfOperator.find('option:first').val(),
+            isBePresent = valueIsIfOperator === firstSelectValue;
+        const CLASS_DISABLED = "disabled";
+        $('option[value=""]').hide();
+        blockOrUnblockFields();
+
+        function blockOrUnblockFields() {
+            blockOrUnlockFieldOperatorNesting();
+            blockOrUnblockFieldConditionOfIfOperator();
+            blockOrUnblockFieldCycleCondition();
+            if ($isOOP.is(':checked')) {
+                blockOrUnlockAllFieldsExceptOne($isOOP);
+            }
+            $isOOP.click(() => blockOrUnlockAllFieldsExceptOne($isOOP));
+            if ($isStrings.is(':checked')) {
+                blockOrUnlockAllFieldsExceptOne($isStrings);
+            }
+            $isStrings.click(() => blockOrUnlockAllFieldsExceptOne($isStrings));
+        }
+
+        function blockOrUnlockFieldOperatorNesting() {
+            let valueIsIfOperator = $isIfOperator.val(),
+                firstSelectValue = $isIfOperator.find('option:first').val(),
+                isIfOperator = valueIsIfOperator === firstSelectValue,
+                checkbox_operator_nesting = $operatorNesting.find('input:checkbox');
+            if (isIfOperator && checkboxCycle.is(':checked')) {
+                unlock($operatorNesting)
+                checkbox_operator_nesting.removeAttr(CLASS_DISABLED);
+            } else {
+                block($operatorNesting);
+                checkbox_operator_nesting.attr(CLASS_DISABLED, true);
+            }
+        }
+
+        function unlock(field) {
+            field.parent().removeClass(CLASS_DISABLED);
+        }
+
+        function block(field) {
+            field.parent().addClass(CLASS_DISABLED);
+        }
+
+        function blockOrUnblockFieldConditionOfIfOperator() {
+            if (isBePresent) {
+                unlock($conditionOfIfOperator);
+            } else {
+                block($conditionOfIfOperator);
+            }
+            $isIfOperator.change(() => {
+                blockOrUnlock($conditionOfIfOperator);
+                blockOrUnlockFieldOperatorNesting();
+            });
+            selectFirstOption($conditionOfIfOperator);
+        }
+
+        function blockOrUnlock(field) {
+            field.parent().toggleClass(CLASS_DISABLED);
+        }
+
+        function selectFirstOption(select) {
+            select.find('option:nth-child(2)').attr('selected', '');
+        }
+
+        function blockOrUnblockFieldCycleCondition() {
+            if (checkboxCycle.is(':checked')) {
+                unlock($cycleCondition);
+            } else {
+                block($cycleCondition);
+            }
+            checkboxCycle.click(() => {
+                blockOrUnlockFieldOperatorNesting();
+                if (checkboxCycle.is(':checked')) {
+                    unlock($cycleCondition);
+                } else {
+                    block($cycleCondition);
+                }
+            })
+        }
+
+        function blockOrUnlockAllFieldsExceptOne($doNotBlockField) {
+            $('.task__body div[id], .task__body select[id], .task__body input[id]').each(function (i, obj) {
+                let $field = $('#' + obj.id),
+                    isCycle = checkboxCycle.is(":checked"),
+                    isContinue = obj.id === $doNotBlockField.attr('id')
+                        || $field.is($cycleCondition) && !isCycle
+                        || $field.is($conditionOfIfOperator) && !isBePresent;
+                if (isContinue) {
+                    return;
+                }
+                if ($field.parent().hasClass(CLASS_DISABLED)) {
+                    unlock($field);
+                } else {
+                    block($field);
+                }
+            });
+        }
+    }
+});
---
+++ src/static/js/testing/testing.edf9f01cbee0.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    let $taskList = $("#task-list");
+    const TIMEOUT = 500;
+
+    putDownNumbering();
+
+    // testing
+    $("#btn-add-task").on("click", blockTasks);
+    $("#btn-update-testing").on("click", blockTasks);
+
+    // task
+    $taskList.on("click", "[name = 'btn-update']", () => {
+        setTimeout(putDownNumbering, TIMEOUT);
+    });
+    $taskList.on("click", ".btn-duplicate", () => {
+        setTimeout(putDownNumbering, TIMEOUT);
+    });
+    $taskList.on("click", ".btn-delete", () => {
+        setTimeout(putDownNumbering, TIMEOUT);
+    });
+
+    // task_form
+    $taskList.on("click", ".btn-update", blockTasks);
+    $taskList.on("click", "#task-form [name = 'btn-update']", unlockTasks);
+    $taskList.on("click", "#btn-add", () => {
+        unlockTasks()
+        setTimeout(putDownNumbering, TIMEOUT);
+    });
+
+    function unlockTasks() {
+        setTimeout(() => {
+            let $error = document.getElementById('error');
+            if (!$error) {
+                const CLASS_DISABLED = 'disabled';
+                $("[name='btns-testing']").removeClass(CLASS_DISABLED);
+                $('.task').removeClass(CLASS_DISABLED);
+                $(":input, a").attr("tabindex", "0");
+                document.removeEventListener('keydown', _blockEnter);
+            }
+        }, TIMEOUT);
+    }
+
+    function blockTasks() {
+        let classDisabled = 'disabled';
+        $("[name='btns-testing']").addClass(classDisabled);
+        $('.task').addClass(classDisabled);
+        $(":input, a").attr("tabindex", "-1");
+        document.addEventListener('keydown', _blockEnter);
+    }
+
+    function _blockEnter(event) {
+        let isEnterPressed = event.keyCode === 13;
+        if (isEnterPressed) {
+            event.preventDefault();
+        }
+    }
+
+    function putDownNumbering() {
+        let $taskNumbers = document.querySelectorAll(".task-number"),
+            numberTask = 1;
+        for (let $taskNumber of $taskNumbers) {
+            $taskNumber.textContent = numberTask.toString();
+            numberTask++;
+        }
+        const COUNT = $taskNumbers.length;
+        _updateCountTasks(COUNT)
+    }
+
+    function _updateCountTasks(count) {
+        let $countTasks = $('#count-tasks');
+        $countTasks.text(count.toString());
+    }
+})
