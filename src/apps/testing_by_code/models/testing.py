f6aa1f7f7523d98768212c4a9fa16d34df8c2653
Rom1987 <maks3564195@gmail.com>
2025-06-30 13:11:25 +0300

    Убрать константу APP_NAME в модели Testing в приложении testing_by_code

--- src/apps/testing_by_code/models/testing.py
+++ src/apps/testing_by_code/models/testing.py
@@ -3,14 +3,13 @@ from django.db.models import Sum, F
 from django.urls import reverse
 
 from abstractions.abstract_models.abstract_testing import AbstractTesting
-from apps.testing_by_code.constants import APP_NAME
 
 
 class Testing(AbstractTesting):
     total_weight = models.IntegerField(default=0, verbose_name='Общий вес')
 
     def get_absolute_url(self) -> reverse:
-        return reverse(APP_NAME + ':testing_detail', kwargs={'pk': self.pk})
+        return reverse('testing_by_code:testing_detail', kwargs={'pk': self.pk})
 
     def get_total_weight(self) -> int:
         return self.task_set.annotate(
