c1673e2cec2f071c2735f64872bd08a5b4f96c8d
Rom1987 <maks3564195@gmail.com>
2025-07-07 15:24:32 +0300

    Убрать поле count в модели Task в приложении testing_by_code

--- src/apps/testing_by_code/models/task.py
+++ src/apps/testing_by_code/models/task.py
@@ -7,10 +7,6 @@ from abstractions.abstract_models import AbstractTask
 class Task(AbstractFieldWeight, AbstractTask):
     RELATED_NAME = 'task_set'
 
-    count = models.IntegerField(
-        default=1,
-        verbose_name='Количество'
-    )
     testing = models.ForeignKey(
         'Testing',
         related_name=RELATED_NAME,
