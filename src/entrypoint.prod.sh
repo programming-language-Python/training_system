b1e1cf5649a9608c1d030a1ae4624452f330eae6
Rom1987 <maks3564195@gmail.com>
2025-07-18 16:46:44 +0300

    Исправить ошибку при долгой генерации задач в Docker
    Теперь выдаёт ошибку 504 и при перезагрузки страницы отображаются задачи

--- src/entrypoint.prod.sh
+++ src/entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application
+python -m gunicorn --timeout 1000 --workers 1 --threads 4 --log-level debug --bind 0.0.0.0:8000 config.wsgi:application
